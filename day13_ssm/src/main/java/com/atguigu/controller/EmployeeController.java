240a2c203715840f1f91fdaf2189a0c5b809d167
wuyang77 <2388958622@qq.com>
2022-10-24 18:30:03 +0800

    分页插件完善，ssm整合结束

--- day13_ssm/src/main/java/com/atguigu/controller/EmployeeController.java
+++ day13_ssm/src/main/java/com/atguigu/controller/EmployeeController.java
@@ -2,21 +2,41 @@ package com.atguigu.controller;
 
 import com.atguigu.pojo.Employee;
 import com.atguigu.service.EmployeeService;
+import com.atguigu.utils.PageUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class EmployeeController {
   @Autowired
   @Qualifier("employeeService")
   private EmployeeService employeeService;
-  @GetMapping("/emps")
-  public  String getAllEmps(Map<String,Object> map){
+  @GetMapping("/emps/{pageNo}")
+  public  String getAllEmps(@PathVariable("pageNo")Integer pageNo,
+                            Map<String,Object> map,
+                            HttpServletRequest request){
+    int pNo = 1;
+    if (pageNo != null){
+      pNo = pageNo;
+    }
+    //开启分页
+    PageHelper.startPage(pNo,5);
+    map.put("pNo",pNo);
     List<Employee> employees = employeeService.getEmployees();
+    //分装分页
+    PageInfo<Employee> pageInfo = new PageInfo<>(employees,5);
+    //使用工具类，获取分页信息
+    String pageRs = PageUtils.getPageInfo(pageInfo,request);
+    //将数据共享到域中
+    map.put("pageRs",pageRs);
     map.put("emps",employees);
     return "emplist";
   }
--- day13_ssm/src/main/java/com/atguigu/utils/PageUtils.java
+++ day13_ssm/src/main/java/com/atguigu/utils/PageUtils.java
@@ -1,5 +1,53 @@
 package com.atguigu.utils;
 
-public class PageUtils {
+import com.atguigu.pojo.Employee;
+import com.github.pagehelper.PageInfo;
+import javax.servlet.http.HttpServletRequest;
 
+public class PageUtils {
+  public static String getPageInfo(PageInfo<Employee> pageInfo, HttpServletRequest request){
+    //String basePath = "/当前项目名/"
+    String basePath = request.getContextPath()+"/";
+
+    //定义结果字符串StringBuilder
+    StringBuilder builder = new StringBuilder();
+
+    //拼接首页
+    builder.append("<a href='"+basePath+"emps/1'>首页</a>");
+    builder.append("&nbsp;&nbsp;");
+
+    //拼接上一页
+    if (pageInfo.isHasPreviousPage()){
+      builder.append("<a href='"+basePath+"emps/"+(pageInfo.getPageNum()-1)+"'>上一页</a>");
+      builder.append("&nbsp;&nbsp;");
+    }else {
+      builder.append("上一页");
+      builder.append("&nbsp;&nbsp;");
+    }
+
+    //拼接页码超链接 1 2 3 4 5
+    int [] nums = pageInfo.getNavigatepageNums();
+    for (int i : nums) {
+      if (i == pageInfo.getPageNum()){
+        builder.append("<a style='text-decoration:none;'href'"+basePath+"emps/");
+        builder.append("&nbsp;&nbsp;");
+      }else {
+        builder.append("<a href='"+basePath+"emps/"+i+"'>"+i+"</a>");
+        builder.append("&nbsp;&nbsp;");
+      }
+    }
+
+    //拼接下一页
+    if (pageInfo.isHasNextPage()){
+      builder.append("<a href='"+basePath+"emps/"+(pageInfo.getPageNum()+1)+"'>下一页</a>");
+      builder.append("&nbsp;&nbsp;");
+    }else {
+      builder.append("下一页");
+      builder.append("&nbsp;&nbsp;");
+    }
+
+    //拼接尾页
+    builder.append("<a href='"+basePath+"emps/"+pageInfo.getPages()+"'>尾页</a>");
+    return builder.toString() ;
+  }
 }
